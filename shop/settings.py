05598186e584a60be25621ce3179659977d330c2
Kirill <54812891+Out-Of-Mind@users.noreply.github.com>
2020-06-30 16:31:55 +0300

    added main app

--- shop/settings.py
+++ shop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
