4191aeb47ae5935564fddf4be74de2209d016916
Kirill <54812891+Out-Of-Mind@users.noreply.github.com>
2020-06-30 16:02:45 +0300

    db config moved to db-config.json

--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,18 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+with open('db-config.json', 'rb') as f:
+    d = json.load(f)
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': d['NAME'],
+'USER': d['USER'],
+'PASSWORD': d['PASSWORD'],
+'HOST': d['HOST'],
+'PORT': d['PORT']
     }
 }
 
